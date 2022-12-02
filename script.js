c0ffdc3614e87afe8b03bdfd6cd6121864b0b9d0
wchizewski <w.chizewski@share.epsb.ca>
2022-12-01 21:28:45 -0700

    home thurs

--- script.js
+++ script.js
@@ -5,91 +5,143 @@ let ctx = cnv.getContext("2d");
 cnv.width = 900;
 cnv.height = 945;
 
-let snakex = 90;
-let snakey = 405;
-let direction;
-let wPressed = false;
-let aPressed = false;
-let sPressed = false;
-let dPressed = false;
 let remainder;
 let lastKeyPressed;
 let score = 0;
 let applex = 450;
 let appley = 405;
+let snakeHead = {
+    x: 90,
+    y: 405,
+    speed: 5,
 
-requestAnimationFrame(loop);
-function loop() {
-    // movement
-    if (lastKeyPressed == "W" || lastKeyPressed == "S") {
-        remainder = snakex % 45;
-    } else {
-        remainder = snakey % 45;
+    move() {
+        this.y += this.vy
+        this.x += this.vx
+    },
+
+    changeDirection() {
+        if (lastKeyPressed == "w" || lastKeyPressed == "s") {
+            remainder = this.x % 45;
+        } else {
+            remainder = this.y % 45;
+        }
+    
+        if (lastKeyPressed == "w" && this.direction != 'down' && remainder == 0) {
+            this.vy = this.speed
+        }
+    
+        if (lastKeyPressed == "s" && this.direction != 'up' && remainder == 0) {
+            this.vy = -this.speed
+        }
+    
+        if (lastKeyPressed == "a" && this.direction != 'right' && remainder == 0) {
+            this.vx = -this.speed
+        }
+    
+        if (lastKeyPressed == "d" && this.direction != 'left' && remainder == 0) {
+            this.vx = this.speed
+        }
+    }
+}
+
+class SnakeSegment {
+    constructor(x, y, index, direction) {
+        this.x = x;
+        this.y = y;
+        this.index = index;
+        this.direction = direction;
     }
 
-    if (lastKeyPressed == "W" && remainder == 0) {
-        direction = "up";
+    move() {
+        if (this.direction == "up") {
+            this.y -= 5;
+        }
+    
+        if (this.direction == "down") {
+            this.y += 5;
+        }
+    
+        if (this.direction == "left") {
+            this.x -= 5;
+        }
+    
+        if (this.direction == "right") {
+            this.x += 5;
+        }
     }
 
-    if (lastKeyPressed == "S" && remainder == 0) {
-        direction = "down";
-    }
+    changeDirection() {
+        let lastDirection = snakeBody[this.index - 1] ? snakeBody[this.index - 1].direction : snakeHead.direction
 
-    if (lastKeyPressed == "A" && remainder == 0) {
-        direction = "left"
+        if (lastDirection == "up" && this.direction != 'down' && remainder == 0) {
+            this.direction = "up";
+        }
+    
+        if (lastDirection == "down" && this.direction != 'up' && remainder == 0) {
+            this.direction = "down";
+        }
+    
+        if (lastDirection == "left" && this.direction != 'right' && remainder == 0) {
+            this.direction = "left"
+        }
+    
+        if (lastDirection == "right" && this.direction != 'left' && remainder == 0) {
+            this.direction = "right"
+        }
     }
+}
 
-    if (lastKeyPressed == "D" && remainder == 0) {
-        direction = "right"
-    }
+let snakeBody = []
 
-    if (direction == "up") {
-        snakey -= 5;
-    }
+document.addEventListener("keydown", keydownHandler)
 
-    if (direction == "down") {
-        snakey += 5;
-    }
+function keydownHandler(event) {
+    lastKeyPressed = event.key;
+}
 
-    if (direction == "left") {
-        snakex -= 5;
-    }
+function reset() {
+    snakeHead.x = 90;
+    snakeHead.y = 405;
+    applex = 450;
+    appley = 405;
+    score = 0
+    snakeHead.direction = "stop"
+    lastKeyPressed = "none"
+}
 
-    if (direction == "right") {
-        snakex += 5;
-    }
-
-    if (snakex > 855) {
-        snakex = 855
+function checkCollision() {
+    if (snakeHead.x > 855) {
+        snakeHead.x = 855
         reset()
     }
 
-    if (snakex < 0) {
-        snakex = 0
+    if (snakeHead.x < 0) {
+        snakeHead.x = 0
         reset()
     }
 
-    if (snakey > 855) {
-        snakey = 855
+    if (snakeHead.y > 855) {
+        snakeHead.y = 855
         reset()
     }
 
-    if (snakey < 0) {
-        snakey = 0
+    if (snakeHead.y < 0) {
+        snakeHead.y = 0
         reset()
     }
 
-
-
-    if (snakex < applex + 45 && snakex + 15 > applex && snakey + 45 > appley && snakey < appley + 45) {
+    if (snakeHead.x < applex + 45 && snakeHead.x + 15 > applex && snakeHead.y + 45 > appley && snakeHead.y < appley + 45) {
         applex = (Math.floor(Math.random() * (20))) * 45;
         appley = (Math.floor(Math.random() * (20))) * 45;
-        score++
-        
+        score++;
+
+        let lastSegment = snakeBody[snakeBody.length - 1] || snakeHead;
+        snakeBody.push(new SnakeSegment(lastSegment.x, lastSegment.y, snakeBody.length - 1, lastSegment.direction))
     }
+}
 
-
-
+function drawGrid() {
     // background
     for (let y = 0; y <= 900; y += 90) {
         ctx.fillStyle = "rgb(93, 201, 38)"
@@ -114,13 +166,36 @@ function loop() {
             ctx.fillRect(x, y, 45, 45)
         }
     }
+}
+
+requestAnimationFrame(loop);
+function loop() {
+    checkCollision()
+
+    snakeHead.changeDirection()
+    for (let i = 0; i < snakeBody.length; i++) {
+        snakeBody[i].changeDirection()
+    }
+
+    snakeHead.move()
+    for (let i = 0; i < snakeBody.length; i++) {
+        snakeBody[i].move()
+    }
+
+    drawGrid()
 
     ctx.fillStyle = "white"
     ctx.fillRect(0, 900, 900, 45)
 
-    // draw square
+    // draw snakeBody
+    for (let i = 0; i < snakeBody.length; i++) {
+        ctx.fillStyle = "rgb(58, 135, 242)"
+        ctx.fillRect(snakeBody[i].x, snakeBody[i].y, 45, 45)
+    }
+
+    // draw snakeHead
     ctx.fillStyle = "rgb(58, 135, 242)"
-    ctx.fillRect(snakex, snakey, 45, 45)
+    ctx.fillRect(snakeHead.x, snakeHead.y, 45, 45)
 
     // draw apple
     ctx.fillStyle = "red"
@@ -134,32 +209,5 @@ function loop() {
     ctx.fillStyle = "black"
     ctx.fillText(score, 45, 940)
 
-
-
-
     requestAnimationFrame(loop);
-}
-
-document.addEventListener("keydown", keydownHandler)
-
-function keydownHandler(event) {
-    if (event.code === "KeyW" && direction !== "down") {
-        lastKeyPressed = "W";
-    } if (event.code === "KeyA" && direction !== "right") {
-        lastKeyPressed = "A";
-    } if (event.code === "KeyS" && direction !== "up") {
-        lastKeyPressed = "S";
-    } if (event.code === "KeyD" && direction !== "left") {
-        lastKeyPressed = "D";
-    }
-}
-
-function reset() {
-    snakex = 90;
-    snakey = 405;
-    applex = 450;
-    appley = 405;
-    score = 0
-    direction = "stop"
-    lastKeyPressed = "none"
 }
